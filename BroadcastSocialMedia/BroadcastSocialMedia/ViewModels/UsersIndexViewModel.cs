fcecf0b342b5a2abe0513ccc63ad6018572b9271
ChrysostomosC <153235540+ChrysostomosC@users.noreply.github.com>
2024-11-28 21:58:32 +0100

    Add search functionality

---
+++ BroadcastSocialMedia/BroadcastSocialMedia/ViewModels/UsersIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BroadcastSocialMedia.Models;
+
+namespace BroadcastSocialMedia.ViewModels
+{
+    public class UsersIndexViewModel
+    {
+        public string Search { get; set; }
+        public List<ApplicationUser> Result { get; set; } = new List<ApplicationUser>(); /* Edw einai pou 8a to orisoume me proepilogh se nea lista */
+    }
+}
